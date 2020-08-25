48b194a5801043910f8c39bd22d853b05b5ab5e8
smpny7 <symphonymode7@icloud.com>
2020-08-25 22:57:17 +0900

    complete

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -1,84 +1,202 @@
+import dotenv from 'dotenv'
 import express from 'express'
 import jwt from 'jsonwebtoken'
-import { CommonDB, UserTable } from './DBManager'
+import { Buffer } from 'buffer';
+import { CommonDB, UserTable, CodeIssuance, ScoreTable } from './dbManager'
 
-// const app = express();
-// const jwt = require("jsonwebtoken")
+const apiRoutes = express.Router()
+const dateformat = require('dateformat')
+const fs = require('fs');
 const router: express.Router = express.Router()
 
+dotenv.config()
 CommonDB.createTableIfNotExists()
 
+// ---- Licence ----
+router.get('/licence', (req: express.Request, res: express.Response) => {
+
+    res.json({
+        success: true,
+        version: JSON.parse(fs.readFileSync('./version.json', 'utf8'))
+    })
+})
+
+// ---- User Registration ----
 router.post('/register', (req: express.Request, res: express.Response) => {
     if (req.body.name) {
         UserTable.existUserCheck(req.body.name)
             .then(bool => {
                 if (bool) {
-                    res.status(500).send('duplicate')
+                    res.status(500).json({
+                        success: false,
+                        msg: 'Duplicate user name'
+                    })
                 } else {
                     UserTable.register(req.body.name)
-                    res.send('success')
+                    const now = new Date()
+                    const payload = {
+                        user: req.body.name,
+                        created_at: dateformat(now, 'yyyy-mm-dd HH:MM:ss')
+                    }
+                    const token = jwt.sign(payload, process.env.APP_KEY as string)
+                    res.json({
+                        success: true,
+                        msg: 'Successfully created account',
+                        token: token
+                    })
                 }
             })
     } else {
-        res.status(500).send('failure')
+        res.status(500).json({
+            success: false,
+            msg: 'Account registration failed'
+        })
     }
 })
 
-const apiRoutes = express.Router()
-// クライアントから送られたIDとパスワード確認してtoken（jwt）発行
-// "/api/authenticate"
-apiRoutes.post("/authenticate", (req, res) => {
-    const payload = {
-        user: "ikep"
-    };
-    const token = jwt.sign(payload, "123456");
-    res.json({
-        success: true,
-        msg: "Authentication successfully finished",
-        token: token
-    });
+// ---- Account Recovery ----
+router.get('/recovery', (req: express.Request, res: express.Response) => {
+    if (req.body.code) {
+        CodeIssuance.getName(req.body.code)
+            .then(name => {
+                CodeIssuance.disableCode(name as string)
+                    .then(row => {
+                        const now = new Date()
+                        const payload = {
+                            user: name,
+                            created_at: dateformat(now, 'yyyy-mm-dd HH:MM:ss')
+                        }
+                        const token = jwt.sign(payload, process.env.APP_KEY as string)
+                        res.json({
+                            success: true,
+                            msg: 'Successfully created account',
+                            token: token
+                        })
+                    })
+            })
+    } else {
+        res.status(500).json({
+            success: false,
+            msg: 'Invalid code'
+        })
+    }
+})
 
-    // IDとパスワードが正しくなかった場合
-    // res.json({
-    //     success: false,
-    //     msg: "Authentication failed"
-    // });
-});
+// ---- Get Top Score ----
+router.get('/topScore', (req, res) => {
+    if (req.body.music) {
+        ScoreTable.topScore(req.body.music)
+            .then(data => {
+                res.json({
+                    success: true,
+                    data: data
+                })
+            })
+    } else {
+        res.status(500).json({
+            success: false,
+            msg: 'Failed getting score'
+        })
+    }
+})
 
-// 一度クライアントに返したtokenが改ざんされずにクライアントから送られてきたか確認
+// ---- Get Top Ten Score ----
+router.get('/topTenScore', (req, res) => {
+    if (req.body.music) {
+        ScoreTable.topTenScore(req.body.music)
+            .then(data => {
+                res.json({
+                    success: true,
+                    data: data
+                })
+            })
+    } else {
+        res.status(500).json({
+            success: false,
+            msg: 'Failed getting score'
+        })
+    }
+})
+
+// ====== JWT authentication required ==========================================
+
 apiRoutes.use((req, res, next) => {
-    var token = req.body.token;
-    // tokenがない場合、アクセスを拒否
+    var token = req.body.token
     if (!token) {
         return res.status(403).send({
             success: false,
-            msg: "No token provided"
-        });
+            msg: 'No token provided'
+        })
     }
-    // tokenが改ざんされていないかチェック
-    jwt.verify(token, "123456", (err: any, decoded: any) => {
-        // tokenが不正なものだった場合、アクセス拒否
+    jwt.verify(token, process.env.APP_KEY as string, (err: any, decoded: any) => {
         if (err) {
-            console.log(err);
             return res.json({
                 success: false,
-                msg: "Invalid token"
-            });
+                msg: 'Invalid token'
+            })
         }
-        // 正しいtokenの場合、認証OKする
-        // req.decoded;
-        next();
-    });
-});
+        next()
+    })
+})
 
-// 認証後、これ以降のURIにアクセス可能となる
-// "/api/private"
-apiRoutes.get("/private", (req, res) => {
-    res.json({
-        msg: "Hello world!"
-    });
-});
+// ---- Get My Score ----
+apiRoutes.get('/myScore', (req, res) => {
+    if (req.body.music) {
+        const jwt = req.body.token.split('.')
+        const decode = JSON.parse(Buffer.from(jwt[1], 'base64').toString())
+        ScoreTable.myScore(req.body.music, decode.user)
+            .then(data => {
+                res.json({
+                    success: true,
+                    data: data
+                })
+            })
+    } else {
+        res.status(500).json({
+            success: false,
+            msg: 'Failed getting score'
+        })
+    }
+})
 
-router.use("/api/", apiRoutes);
+// ---- Score Registration ----
+apiRoutes.post('/registScore', (req, res) => {
+    if (req.body.music && req.body.score) {
+        const jwt = req.body.token.split('.')
+        const decode = JSON.parse(Buffer.from(jwt[1], 'base64').toString())
+        ScoreTable.register(req.body.music, decode.user, req.body.score)
+        res.json({
+            success: true,
+            msg: 'Successfully registerd score'
+        })
+    } else {
+        res.status(500).json({
+            success: false,
+            msg: 'Score registration failed'
+        })
+    }
+})
+
+// ---- Code Generation ----
+apiRoutes.get('/issuingAccountCode', (req, res) => {
+    const jwt = req.body.token.split('.')
+    const decode = JSON.parse(Buffer.from(jwt[1], 'base64').toString())
+    CodeIssuance.disableCode(decode.user)
+        .then(result => {
+            CodeIssuance.codeIssuance(decode.user)
+                .then(code => {
+                    res.json({
+                        success: true,
+                        msg: 'Successfully created code',
+                        code: code
+                    })
+                })
+        })
+})
+
+// =============================================================================
+
+
+router.use("/auth/", apiRoutes)
 
 module.exports = router
