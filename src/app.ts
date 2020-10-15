2cf7f570bf00c41395014c3de5457ff2909bbf62
smpny7 <symphonymode7@icloud.com>
2020-10-15 12:16:06 +0900

    add development/production switching

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 
 const app: express.Express = express()
 const dbm = require('db-migrate').getInstance(true)
+const dotenv = require('dotenv').config()
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
@@ -16,14 +17,19 @@ const router3_0: express.Router = require('./v3.0/')
 
 app.use('/v3.0/', router3_0)
 
-setTimeout(() => {
-    require('greenlock-express')
-        .init({
-            packageRoot: __dirname + '/../',
-            maintainerEmail: "developer_ikep@gmail.com",
-            configDir: './greenlock.d',
-            cluster: false
-        })
-        .serve(app);
+if (dotenv.parsed.TARGET_ENV == "development") {
+    app.listen(3000, () => { console.log('[INFO] Listening on port 3000...') })
     dbm.up()
-}, 30000)
+} else if (dotenv.parsed.TARGET_ENV == "production") {
+    setTimeout(() => {
+        require('greenlock-express')
+            .init({
+                packageRoot: __dirname + '/../',
+                maintainerEmail: "developer_ikep@gmail.com",
+                configDir: './greenlock.d',
+                cluster: false
+            })
+            .serve(app);
+        dbm.up()
+    }, 30000)
+}
